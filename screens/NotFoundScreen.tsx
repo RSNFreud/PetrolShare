68ce9b13c96b4f32d99abaec27808af6d826dd12
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2022-08-30 05:02:54 +0300

    update app.json and NotFoundScreen.tsx

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -1,3 +1,4 @@
+import { useRoute } from "@react-navigation/native";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 
 import { Text } from "../components/Themed";
@@ -6,11 +7,15 @@ import { RootStackScreenProps } from "../types";
 export default function NotFoundScreen({
   navigation,
 }: RootStackScreenProps<"NotFound">) {
+  const route = useRoute();
+  console.log(route.path);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>This screen doesn't exist.</Text>
+      <Text>{route.path}</Text>
       <TouchableOpacity
-        onPress={() => navigation.replace("Root")}
+        onPress={() => navigation.popToTop()}
         style={styles.link}
       >
         <Text style={styles.linkText}>Go to home screen!</Text>
