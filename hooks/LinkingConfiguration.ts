9edf3645aab7ab1eec7a0325cba8016a62b97d74
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2022-10-16 16:53:56 +0300

    update app.json and LinkingConfiguration.ts

--- hooks/LinkingConfiguration.ts
+++ hooks/LinkingConfiguration.ts
@@ -5,6 +5,7 @@
  */
 
 import * as Linking from 'expo-linking'
+import * as Notifications from 'expo-notifications';
 
 const linking: any = {
   prefixes: [Linking.createURL('/')],
@@ -23,6 +24,37 @@ const linking: any = {
       GPS: 'gps-tracking',
       DesktopScreen: '/',
     },
+  }, async getInitialURL() {
+    // First, you may want to do the default deep link handling
+    // Check if app was opened from a deep link
+    let url = await Linking.getInitialURL();
+
+    if (url != null) {
+      return url;
+    }
+
+    // Handle URL from expo push notifications
+    const response = await Notifications.getLastNotificationResponseAsync();
+    url = response?.notification.request.content.data.url as string;
+
+    return url;
+  }, subscribe(listener) {
+    const onReceiveURL = ({ url }: { url: string }) => listener(url);
+
+    // Listen to incoming links from deep linking
+    Linking.addEventListener('url', onReceiveURL);
+
+    // Listen to expo push notifications
+    const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+      const url = response.notification.request.content.data.url;
+      listener(url);
+    });
+
+    return () => {
+      // Clean up the event listeners
+      Linking.removeEventListener('url', onReceiveURL);
+      subscription.remove();
+    };
   },
 }
 
