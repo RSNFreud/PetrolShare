8fa386e4902c95297f521473ff4c65df2878d838
rsnfreud <23102186+RSNFreud@users.noreply.github.com>
2022-11-09 17:45:14 +0200

    update App.tsx, screens/dashboard/index.tsx and types.tsx

--- types.tsx
+++ types.tsx
@@ -3,42 +3,47 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
 import {
   CompositeScreenProps,
   NavigatorScreenParams,
-} from "@react-navigation/native";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+} from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 declare global {
   namespace ReactNavigation {
     interface RootParamList extends RootStackParamList {}
+    // interface Params extends Route {
+    //   groupID?: string
+    // }
   }
 }
 
 export type RootStackParamList = {
-  Settings: NavigatorScreenParams<RootTabParamList> | undefined;
-  AddDistance: NavigatorScreenParams<RootTabParamList> | undefined;
-  AddPetrol: NavigatorScreenParams<RootTabParamList> | undefined;
-  AddPreset: NavigatorScreenParams<RootTabParamList> | undefined;
-  Logs: NavigatorScreenParams<RootTabParamList> | undefined;
-  AddManual: NavigatorScreenParams<RootTabParamList> | undefined;
-  Invoices: NavigatorScreenParams<RootTabParamList> | undefined;
-  Login: NavigatorScreenParams<RootTabParamList> | undefined;
-  Dashboard: NavigatorScreenParams<RootTabParamList> | undefined;
-  NotFound: undefined;
-};
+  Settings: NavigatorScreenParams<RootTabParamList> | undefined
+  AddDistance: NavigatorScreenParams<RootTabParamList> | undefined
+  AddPetrol: NavigatorScreenParams<RootTabParamList> | undefined
+  AddPreset: NavigatorScreenParams<RootTabParamList> | undefined
+  Logs: NavigatorScreenParams<RootTabParamList> | undefined
+  AddManual: NavigatorScreenParams<RootTabParamList> | undefined
+  Invoices: NavigatorScreenParams<RootTabParamList> | undefined
+  Login: NavigatorScreenParams<RootTabParamList> | undefined
+  Dashboard: NavigatorScreenParams<RootTabParamList> | undefined
+  NotFound: undefined
+}
 
-export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
-  NativeStackScreenProps<RootStackParamList, Screen>;
+export type RootStackScreenProps<
+  Screen extends keyof RootStackParamList
+> = NativeStackScreenProps<RootStackParamList, Screen>
 
 export type RootTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
-};
+  Dashboard: { groupID?: string }
+  TabTwo: undefined
+}
 
-export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
-  CompositeScreenProps<
-    BottomTabScreenProps<RootTabParamList, Screen>,
-    NativeStackScreenProps<RootStackParamList>
-  >;
+export type RootTabScreenProps<
+  Screen extends keyof RootTabParamList
+> = CompositeScreenProps<
+  BottomTabScreenProps<RootTabParamList, Screen>,
+  NativeStackScreenProps<RootStackParamList>
+>
