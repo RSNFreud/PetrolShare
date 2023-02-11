0c68a0578005c86e7ffe09b550f8355c3c75731a
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2023-02-11 20:15:55 +0200

    update assignDistance.tsx

--- components/assignDistance.tsx
+++ components/assignDistance.tsx
@@ -16,13 +16,23 @@ type PropsType = {
 }
 
 export default ({ active, data, handleClose, invoiceID, handleUpdate }: PropsType) => {
-    const usernames = Object.entries(data).map(([key, e]) => ({ name: e.fullName, value: key })).filter(e => e.name !== "Unaccounted Distance")
+    const [usernames, setUsernames] = useState<Array<string>>([])
     let maxDistance: { fullName: string, distance: number } | number = Object.values(data).filter(e => e.fullName === "Unaccounted Distance")[0]
     if (maxDistance) maxDistance = maxDistance.distance
     const [errors, setErrors] = useState({ name: "", distance: "" })
     const [values, setValues] = useState({ name: "", distance: "" })
     const [loading, setLoading] = useState(false)
     const { retrieveData } = useContext(AuthContext);
+    useEffect(() => {
+        axios
+            .get(config.REACT_APP_API_ADDRESS + `/group/get-members?authenticationKey=` + retrieveData().authenticationKey)
+            .then(async ({ data }) => {
+                setUsernames(data.map((e: { fullName: string, userID: string }) => ({ name: e.fullName, value: e.userID })))
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+    }, [])
 
     const submit = () => {
         setLoading(false)
@@ -64,7 +74,8 @@ export default ({ active, data, handleClose, invoiceID, handleUpdate }: PropsTyp
 
     return <Popup visible={active} handleClose={handleClose}>
         <Input handleInput={e => setValues({ ...values, distance: e })} label={`Distance to apply`} errorMessage={errors.distance} placeholder={`Enter amount (Max: ${maxDistance})`} keyboardType={'numbers-and-punctuation'} inputStyle={{ paddingVertical: 10 }} style={{ marginBottom: 20, marginTop: 20 }} />
-        <Dropdown label="User" placeholder="Choose a username" data={usernames} handleSelected={e => setValues({ ...values, name: e })} value={values.name} errorMessage={errors.name} hiddenValue />
+        {Boolean(usernames.length) ?
+            <Dropdown label="User" placeholder="Choose a username" data={usernames} handleSelected={e => setValues({ ...values, name: e })} value={values.name} errorMessage={errors.name} hiddenValue /> : <></>}
         <Button loading={loading} handleClick={submit}>Assign Distance</Button>
     </Popup>
 }
