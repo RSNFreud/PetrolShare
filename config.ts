f12557c32b5c478fcd2a05468d6731f2727c94ec
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2023-07-23 02:59:03 +0300

    update config.ts

--- config.ts
+++ config.ts
@@ -2,6 +2,6 @@ const REACT_APP_ADDRESS = "https://petrolshare.freud-online.co.uk/";
 
 export default {
     REACT_APP_ADDRESS: REACT_APP_ADDRESS,
-    REACT_APP_API_ADDRESS: `http://10.0.2.2:3435/beta/api`,
+    REACT_APP_API_ADDRESS: `http://localhost:3435/beta/api`,
     REACT_APP_EMAIL_API_ADDRESS: `https://petrolshare.freud-online.co.uk/email`
 };
