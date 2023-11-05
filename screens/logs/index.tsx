188d3b78ab936e4f964624792b163e99395c81bc
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2023-11-05 20:12:44 +0200

    update screens/logs/index.tsx

--- screens/logs/index.tsx
+++ screens/logs/index.tsx
@@ -120,7 +120,7 @@ export default () => {
         links={[
           {
             name: "Dashboard",
-            screenName: "Home",
+            screenName: "",
           },
           {
             name: "Logs",
