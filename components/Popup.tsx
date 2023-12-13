a2b79ca9d8d6e3cbd956490bf0230cb5763ff60d
rsnfreud <theo@freud-online.co.uk>
2023-12-13 16:47:28 +0200

    update App.tsx, Popup.tsx and screens/login/index.tsx

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -10,59 +10,87 @@ import {
   Pressable,
   ScrollView,
   View,
-} from "react-native";
-import { useEffect, useRef, useState } from "react";
-import Colors from "../constants/Colors";
-import AlertBox from "./alertBox";
-import { sendCustomEvent } from "../hooks";
-import { Text } from "./text";
-import Exit from "../assets/icons/exit";
+} from 'react-native'
+import { useEffect, useRef, useState } from 'react'
+import Colors from '../constants/Colors'
+import AlertBox from './alertBox'
+import { sendCustomEvent } from '../hooks'
+import { Text } from './text'
+import Exit from '../assets/icons/exit'
 
-const TIME_TO_CLOSE = 200;
+const TIME_TO_CLOSE = 200
 
 type ModalType = {
-  visible: boolean;
-  handleClose: () => void;
-  children: JSX.Element | Array<JSX.Element>;
-  height?: DimensionValue;
-  animate?: boolean;
-  showClose?: boolean;
-  title?: string;
-};
+  visible: boolean
+  handleClose: () => void
+  children: JSX.Element | Array<JSX.Element>
+  height?: DimensionValue
+  animate?: boolean
+  showClose?: boolean
+  title?: string
+}
 
 export default ({
   visible,
   handleClose,
   children,
   showClose = true,
-  height = "auto",
+  height = 'auto',
   animate = true,
   title,
 }: ModalType) => {
-  const [opened, setOpened] = useState(false);
-  const [modalHeight, setModalHeight] = useState(0);
-  const [isVisible, setIsVisible] = useState(false);
-  const [keyboardPadding, setKeyboardPadding] = useState(0);
-  let position = useRef(new Animated.Value(modalHeight)).current;
+  const [opened, setOpened] = useState(false)
+  const [modalHeight, setModalHeight] = useState(0)
+  const [isVisible, setIsVisible] = useState(false)
+  const [keyboardPadding, setKeyboardPadding] = useState(0)
+  let position = useRef(new Animated.Value(modalHeight)).current
+  let maxHeight = useRef(
+    new Animated.Value(Dimensions.get('window').height * 0.9 - keyboardPadding),
+  ).current
+  const containerRef = useRef<View>(null)
 
-  const getHeight = (event: LayoutChangeEvent) => {
-    setModalHeight(event?.nativeEvent?.layout.height);
-  };
+  const getHeight = () => {
+    if (keyboardPadding > 0) return
+    containerRef.current?.measure((_x, _y, _width, height) => {
+      setModalHeight(height)
+    })
+  }
 
   useEffect(() => {
-    const showSubscription = Keyboard.addListener("keyboardDidShow", (e) => {
-      if (Platform.OS !== "ios") return;
-      setKeyboardPadding(e.endCoordinates.height);
-    });
-    const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
-      setKeyboardPadding(0);
-    });
+    console.log(60, keyboardPadding)
+  }, [keyboardPadding])
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener('keyboardWillShow', (e) => {
+      if (Platform.OS !== 'ios') return
+      setKeyboardPadding(e.endCoordinates.height)
+      Animated.sequence([
+        Animated.timing(position, {
+          toValue: -e.endCoordinates.height,
+          duration: TIME_TO_CLOSE,
+          delay: 200,
+          useNativeDriver: true,
+        }),
+      ]).start()
+    })
+    const hideSubscription = Keyboard.addListener('keyboardWillHide', () => {
+      if (Platform.OS !== 'ios') return
+      Animated.sequence([
+        Animated.timing(position, {
+          toValue: 0,
+          delay: 200,
+          duration: TIME_TO_CLOSE,
+          useNativeDriver: true,
+        }),
+      ]).start()
+      // setKeyboardPadding(0)
+    })
 
     return () => {
-      showSubscription.remove();
-      hideSubscription.remove();
-    };
-  }, []);
+      showSubscription.remove()
+      hideSubscription.remove()
+    }
+  }, [keyboardPadding])
 
   const open = () => {
     Animated.sequence([
@@ -72,58 +100,58 @@ export default ({
         delay: 200,
         useNativeDriver: true,
       }),
-    ]).start((e) => {
-      if (isVisible || visible) setOpened(true);
-    });
-  };
+    ]).start((_e) => {
+      if (isVisible || visible) setOpened(true)
+    })
+  }
 
   const close = () => {
-    position.setValue(0);
-    setOpened(true);
+    position.setValue(0)
+    setOpened(true)
     Animated.sequence([
       Animated.timing(position, {
         toValue: modalHeight,
         duration: TIME_TO_CLOSE,
         useNativeDriver: true,
       }),
-    ]).start((e) => {
-      setIsVisible(false);
-      setOpened(false);
+    ]).start((_e) => {
+      setIsVisible(false)
+      setOpened(false)
       setTimeout(() => {
-        handleClose();
-        sendCustomEvent("popupVisible", visible);
-      }, 300);
-    });
-  };
+        handleClose()
+        sendCustomEvent('popupVisible', visible)
+      }, 300)
+    })
+  }
 
   useEffect(() => {
-    if (!isVisible || opened) return;
-    position.setValue(modalHeight || 1000);
+    if (!isVisible || opened) return
+    position.setValue(modalHeight || 1000)
 
-    if (!animate) return position.setValue(0);
-    if (modalHeight >= 1) open();
-  }, [isVisible, modalHeight]);
+    if (!animate) return position.setValue(0)
+    if (modalHeight >= 1) open()
+  }, [isVisible, modalHeight])
 
   useEffect(() => {
     if (!visible && isVisible) {
       try {
-        Keyboard.dismiss();
+        Keyboard.dismiss()
       } catch {}
       setTimeout(() => {
-        close();
-      }, 400); // Hide keyboard
-      return;
+        close()
+      }, 400) // Hide keyboard
+      return
     }
-    sendCustomEvent("popupVisible", visible);
-    setIsVisible(visible);
-  }, [visible]);
+    sendCustomEvent('popupVisible', visible)
+    setIsVisible(visible)
+  }, [visible])
 
   return (
     <Modal
       animationType="fade"
       visible={isVisible}
       transparent={true}
-      accessibilityLabel={"popup"}
+      accessibilityLabel={'popup'}
       onRequestClose={() => showClose && close()}
     >
       <AlertBox />
@@ -131,41 +159,42 @@ export default ({
         onPress={() => showClose && close()}
         android_disableSound={true}
         style={{
-          backgroundColor: "rgba(35, 35, 35, 0.8)",
-          height: Dimensions.get("window").height,
+          backgroundColor: 'rgba(35, 35, 35, 0.8)',
+          height: Dimensions.get('window').height,
         }}
       />
       <Animated.View
-        onLayout={(e) => getHeight(e)}
+        ref={containerRef}
+        onLayout={getHeight}
         style={{
           backgroundColor: Colors.secondary,
           height: height,
           borderTopLeftRadius: 8,
           borderTopRightRadius: 8,
           transform: [{ translateY: position }],
-          position: "absolute",
+          position: 'absolute',
           bottom: 0,
-          width: "100%",
-          maxHeight: Dimensions.get("window").height * 0.9,
+          width: '100%',
+          maxHeight: Dimensions.get('window').height * 0.9 - keyboardPadding,
           zIndex: 2,
-          display: "flex",
-          flexDirection: "column",
-          borderStyle: "solid",
+          display: 'flex',
+          flexDirection: 'column',
+          borderStyle: 'solid',
           borderWidth: 1,
           borderColor: Colors.border,
         }}
       >
         <View
           style={{
-            position: "absolute",
+            position: 'absolute',
             right: 0,
             top: 0,
             zIndex: 1,
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            width: "100%",
-            flexDirection: "row",
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+            flexDirection: 'row',
             paddingHorizontal: 25,
             paddingVertical: 5,
             borderBottomWidth: 1,
@@ -175,21 +204,21 @@ export default ({
         >
           <View>
             {!!title && (
-              <Text style={{ fontSize: 16, fontWeight: "bold" }}>{title} </Text>
+              <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{title} </Text>
             )}
           </View>
           {showClose && (
             <Pressable
               android_disableSound={true}
               onPress={close}
-              accessibilityHint={"closes the popup"}
+              accessibilityHint={'closes the popup'}
               style={{
                 width: 40,
                 height: 40,
-                alignContent: "center",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
+                alignContent: 'center',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
               <Exit />
@@ -197,9 +226,7 @@ export default ({
           )}
         </View>
         <ScrollView
-          keyboardShouldPersistTaps={"always"}
-          automaticallyAdjustKeyboardInsets
-          contentInset={{ bottom: keyboardPadding }}
+          keyboardShouldPersistTaps={'always'}
           style={{
             marginTop: 51,
             flex: 1,
@@ -213,5 +240,5 @@ export default ({
         </ScrollView>
       </Animated.View>
     </Modal>
-  );
-};
+  )
+}
