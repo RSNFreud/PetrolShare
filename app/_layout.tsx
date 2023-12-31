19728c720dcbdd6df9a2b35723678b3e7d14dba7
rsnfreud <theo@freud-online.co.uk>
2023-12-31 17:56:38 +0200

    build(deps): update package.json and yarn.lock

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import Header from '@components/Header'
+import { Stack } from 'expo-router'
+
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <Stack />
+    </>
+  )
+}
