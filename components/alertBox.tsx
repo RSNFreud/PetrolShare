164d28898f84284182c0d870b1fdf4b7e1dcb877
rsnfreud <theo@freud-online.co.uk>
2024-03-07 17:36:23 +0200

    update alertBox.tsx

--- components/alertBox.tsx
+++ components/alertBox.tsx
@@ -1,50 +1,54 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 import {
   Dimensions,
   Modal,
   Pressable,
   TouchableWithoutFeedback,
   View,
-} from "react-native";
-import { EventRegister } from "react-native-event-listeners";
+} from 'react-native'
+import { EventRegister } from 'react-native-event-listeners'
 
-import { Text } from "./text";
-import Colors from "../constants/Colors";
+import { Text } from './text'
+import Colors from '../constants/Colors'
+import { sendCustomEvent } from 'hooks'
 
 export type ButtonType = {
-  text: string;
-  onPress?: ((value?: string) => void) | undefined;
-  style?: "default" | "cancel" | undefined;
-};
+  text: string
+  onPress?: ((value?: string) => void) | undefined
+  style?: 'default' | 'cancel' | undefined
+}
 
 type AlertDataType = {
-  title?: string;
-  message?: string;
-  buttons?: ButtonType[];
-};
+  title?: string
+  message?: string
+  buttons?: ButtonType[]
+}
 
 export default () => {
-  const [visible, setVisible] = useState(false);
-  const [data, setData] = useState<AlertDataType>();
+  const [visible, setVisible] = useState(false)
+  const [data, setData] = useState<AlertDataType>()
 
   const close = () => {
-    setVisible(false);
-  };
+    sendCustomEvent('closeAlert')
+  }
 
   useEffect(() => {
-    EventRegister.addEventListener("openAlert", (e: AlertDataType) => {
-      setData(e);
-      setVisible(true);
-    });
+    EventRegister.addEventListener('openAlert', (e: AlertDataType) => {
+      setData(e)
+      setVisible(true)
+    })
+    EventRegister.addEventListener('closeAlert', (e: AlertDataType) => {
+      setVisible(false)
+    })
     return () => {
-      EventRegister.removeEventListener("openAlert");
-    };
-  }, []);
+      EventRegister.removeEventListener('openAlert')
+    }
+  }, [])
 
   const handleButtonClick = (button: ButtonType) => {
-    setVisible(false);
-    button.onPress && button.onPress();
-  };
+    close()
+    button.onPress && button.onPress()
+  }
 
   return (
     <Modal
@@ -52,44 +56,44 @@ export default () => {
       animationType="fade"
       transparent
       accessibilityLabel="alertBox"
-      style={{ width: Dimensions.get("window").width }}
+      style={{ width: Dimensions.get('window').width }}
     >
       <View
         style={{
-          display: "flex",
-          alignItems: "center",
-          maxWidth: Dimensions.get("window").width,
-          justifyContent: "center",
+          display: 'flex',
+          alignItems: 'center',
+          maxWidth: Dimensions.get('window').width,
+          justifyContent: 'center',
           flex: 1,
-          alignContent: "center",
+          alignContent: 'center',
         }}
       >
         <Pressable
           onPress={close}
           android_disableSound
           style={{
-            backgroundColor: "rgba(35, 35, 35, 0.8)",
+            backgroundColor: 'rgba(35, 35, 35, 0.8)',
             zIndex: 2,
-            width: "100%",
-            height: Dimensions.get("window").height,
-            position: "absolute",
+            width: '100%',
+            height: Dimensions.get('window').height,
+            position: 'absolute',
           }}
         />
         <View
           style={{
             paddingHorizontal: 25,
             zIndex: 4,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            alignContent: "center",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            alignContent: 'center',
           }}
         >
           <View
             style={{
               backgroundColor: Colors.secondary,
               borderRadius: 8,
-              borderStyle: "solid",
+              borderStyle: 'solid',
               padding: 20,
               borderWidth: 1,
               borderColor: Colors.border,
@@ -97,7 +101,7 @@ export default () => {
             }}
           >
             {!!data?.title && (
-              <Text style={{ fontWeight: "bold", lineHeight: 24 }}>
+              <Text style={{ fontWeight: 'bold', lineHeight: 24 }}>
                 {data.title}
               </Text>
             )}
@@ -109,10 +113,10 @@ export default () => {
             <View
               style={{
                 marginTop: 20,
-                display: "flex",
-                flexDirection: "row",
+                display: 'flex',
+                flexDirection: 'row',
                 gap: 30,
-                justifyContent: "flex-end",
+                justifyContent: 'flex-end',
               }}
             >
               {data?.buttons?.map((button) => (
@@ -123,8 +127,8 @@ export default () => {
                   <Text
                     style={{
                       color: Colors.tertiary,
-                      fontWeight: "bold",
-                      textTransform: "uppercase",
+                      fontWeight: 'bold',
+                      textTransform: 'uppercase',
                     }}
                   >
                     {button.text}
@@ -136,8 +140,8 @@ export default () => {
                   <Text
                     style={{
                       color: Colors.tertiary,
-                      fontWeight: "bold",
-                      textTransform: "uppercase",
+                      fontWeight: 'bold',
+                      textTransform: 'uppercase',
                     }}
                   >
                     Close
@@ -149,5 +153,5 @@ export default () => {
         </View>
       </View>
     </Modal>
-  );
-};
+  )
+}
